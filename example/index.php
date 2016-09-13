f9bccbe5d93af5a2b8416fc6f9c82eff92f8c823
Amit Kumar Khare <amitkhare@users.noreply.github.com>
2016-09-13 18:37:20 +0530

    Create index.php

---
+++ example/index.php
@@ -0,0 +1,20 @@
+<?php
+    use AmitKhare\ValidBit; // use namespace.
+    // autoload via composer
+    require __DIR__.'/../vendor/autoload.php';
+    // OR
+    // require("PATH-TO/"."validbit.php"); // only need to include if installed manually.
+    
+    $v = new ValidBit(); // instantiate ValidBit;
+	//  OR with database for unique field check
+    $v = new ValidBit($host,$username,$password,$dbname); // instantiate ValidBit With Database features;
+    $v->setSource($_POST); // set data source array;
+    
+    $v->check("mobile","required|numeric|min:10|max:15");
+    $v->check("username","required|alphanum|unique:users.username|min:4|max:20");
+    $v->check("email","required|email|unique:users.email|min:4|max:100");
+    if($v->isValid()){
+    	echo "PASS";
+    } else {
+        print_r($v->getStatus());
+    }
